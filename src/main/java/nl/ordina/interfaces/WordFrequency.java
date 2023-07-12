14ab1dbbd30cf019ba6bca154f925a4186c94efc
Jaan Van Gils <j.j.g.vangils@students.uu.nl>
2023-07-12 13:41:17 +0200

    Add required interfaces

---
+++ src/main/java/nl/ordina/interfaces/WordFrequency.java
@@ -0,0 +1,6 @@
+package nl.ordina.interfaces;
+
+public interface WordFrequency {
+    String getWord();
+    int getFrequency();
+}
---
+++ src/main/java/nl/ordina/interfaces/WordFrequencyAnalyzer.java
@@ -0,0 +1,8 @@
+package nl.ordina.interfaces;
+import java.util.List;
+
+public interface WordFrequencyAnalyzer {
+    int calculateHighestFrequency(String text);
+    int calculateFrequencyForWord (String text, String word);
+    List<WordFrequency> calculateMostFrequentNWords (String text, int n);
+}
